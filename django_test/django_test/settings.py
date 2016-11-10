a77b964e6a019d263447c6fd7f09994ec5b58c24
Cappadocia <530524060@qq.com>
2016-11-10 16:13:53 +0800

    增加mysql 设置

--- django_test/django_test/settings.py
+++ django_test/django_test/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'web_test',
 ]
 
 MIDDLEWARE = [
@@ -75,8 +76,12 @@ WSGI_APPLICATION = 'django_test.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    'ENGINE': 'django.db.backends.mysql',
+    'NAME':'08day5',
+    'USER': 'root',
+    'PASSWORD': 'hello123!',
+    'HOST': '192.168.77.119',
+    'PORT': '3306',
     }
 }
 
