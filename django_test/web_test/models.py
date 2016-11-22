3dc1236710687326619a757aa28fd53f20cda7bc
Cappadocia <530524060@qq.com>
2016-11-22 15:40:38 +0800

    增加usertype表

--- django_test/web_test/models.py
+++ django_test/web_test/models.py
@@ -2,11 +2,14 @@ from django.db import models
 
 # Create your models here.
 
+class UserType(models.Model):
+    name = models.CharField(max_length=50)
 
 class UserInfo(models.Model):
     username = models.CharField(max_length=50)
     password = models.CharField(max_length=50)
-    memo = models.CharField(max_length=50)
-    gender = models.BooleanField()
-    age = models.IntegerField()
-    createdate = models.DateField()
+    memo = models.TextField(default = 'xxxx')
+    gender = models.BooleanField(default = False)
+    Age = models.IntegerField(default = 19)
+    createdate = models.DateField(default = '2016-11-23')
+    typeId = models.ForeignKey(UserType)
