fe697f699cc58808f37bb703e3816698533022ce
Cappadocia <530524060@qq.com>
2016-11-22 15:59:26 +0800

    新增 user  gourp  user_group_relation表

--- django_test/web_test/models.py
+++ django_test/web_test/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.db.models.fields.related import OneToOneField
 
 # Create your models here.
 
@@ -13,3 +14,14 @@ class UserInfo(models.Model):
     Age = models.IntegerField(default = 19)
     createdate = models.DateField(default = '2016-11-23')
     typeId = models.ForeignKey(UserType)
+    
+    
+class Group(models.Model):
+    name = models.CharField(max_length=50)
+    
+class User(models.Model):
+    name = models.CharField(max_length=50)
+    email = models.CharField(max_length=50)
+    #建立Group和User表多对多的关系
+    group_relation = models.ManyToManyField('Group')
+    #一对一表关系OneToOneField
